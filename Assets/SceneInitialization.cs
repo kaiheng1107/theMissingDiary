db2dad462cbc922eb1e4ef616be53553a748070c
kaiheng1107 <kaiheng1107@gmail.com>
2020-10-20 01:18:15 +0800

    none

---
+++ Assets/SceneInitialization.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneInitialization : MonoBehaviour
+{
+    [SerializeField]
+    private MySceneManager m_SceneManager;
+
+    private void Awake()
+    {
+        if (m_SceneManager != null)
+            Instantiate(m_SceneManager);
+
+        DontDestroyOnLoad(MySceneManager.Instance);
+    }
+}
