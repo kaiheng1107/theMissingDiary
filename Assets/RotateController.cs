12dc6953affccbf1aace540a7a5e1ef3867afc19
kaiheng1107 <kaiheng1107@gmail.com>
2020-10-20 01:17:48 +0800

    none

---
+++ Assets/RotateController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    private void Update()
+    {
+        var rotation = transform.rotation * Quaternion.AngleAxis(90.0f, Vector3.up);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, speed * Time.deltaTime);
+    }
+}
